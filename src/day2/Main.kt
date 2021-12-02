12fd7328d79a03baf4d9bd05e0a78411291cd7ba
Zach Thacker <zpthacker@gmail.com>
2021-12-02 00:38:51 -0500

    day 2 refined

--- src/day2/Main.kt
+++ src/day2/Main.kt
@@ -4,24 +4,66 @@ import com.zpthacker.aoc21.getInputLines
 
 fun main() {
     val lines = getInputLines(2)
-    var horizontal = 0
-    var depth = 0
-    var aim = 0
-    lines.forEach {
-        val tokens = it.split(" ")
-        val distance = tokens[1].toInt()
-        when (tokens[0]) {
-            "forward" -> {
-                horizontal += distance
-                depth += aim * distance
+    val part1Answer = NaiveSubPosition().performInstructions(lines).answerKey
+    val part2Answer = AdvancedSubPosition().performInstructions(lines).answerKey
+    println("Day 2: Dive!")
+    println("Part 1 answer: $part1Answer")
+    println("Part 2 answer: $part2Answer")
+}
+
+abstract class SubPosition {
+    protected var horizontal: Int = 0
+    protected var depth: Int = 0
+    val answerKey: Int
+        get() = horizontal * depth
+
+    fun performInstructions(instructions: List<String>): SubPosition {
+        instructions
+            .forEach {
+                val tokens = it.split(" ")
+                val command = tokens[0]
+                val argument = tokens[1].toInt()
+                when (command) {
+                    "forward" -> forward(argument)
+                    "up" -> up(argument)
+                    "down" -> down(argument)
+                }
             }
-            "up" -> {
-                aim -= distance
-            }
-            "down" -> {
-                aim += distance
-            }
-        }
+        return this
     }
-    println(horizontal * depth)
+
+    protected abstract fun forward(distance: Int)
+    protected abstract fun up(distance: Int)
+    protected abstract fun down(distance: Int)
+}
+
+class NaiveSubPosition: SubPosition() {
+    override fun forward(distance: Int) {
+        horizontal += distance
+    }
+
+    override fun up(distance: Int) {
+        depth -= distance
+    }
+
+    override fun down(distance: Int) {
+        depth += distance
+    }
+}
+
+class AdvancedSubPosition: SubPosition() {
+    private var aim: Int = 0
+
+    override fun forward(distance: Int) {
+        horizontal += distance
+        depth += distance * aim
+    }
+
+    override fun up(distance: Int) {
+        aim -= distance
+    }
+
+    override fun down(distance: Int) {
+        aim += distance
+    }
 }
