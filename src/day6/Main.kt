4660e70d48d4a4b842edb57f2ee5b7c8e8e4c40c
Zach Thacker <zpthacker@gmail.com>
2021-12-06 00:19:55 -0500

    Day 6 refined

--- src/day6/Main.kt
+++ src/day6/Main.kt
@@ -3,30 +3,27 @@ package com.zpthacker.aoc21.day6
 import com.zpthacker.aoc21.getInputLines
 
 fun main() {
-    val lines = getInputLines(6)
-//    val lines = """
-//       3,4,3,1,2
-//    """.trimIndent().split("\n")
-    val fish = lines.first().split(",").map(String::toLong)
-    val min = fish.minOrNull()!!
-    val max = fish.maxOrNull()!!
+    val initialFish = getInputLines(6)
+        .first()
+        .split(",")
+        .map(String::toInt)
     val groupedFish = MutableList(9) { 0L }
-    fish.forEach {
-        groupedFish[it.toInt()]++
+    initialFish.forEach {
+        groupedFish[it]++
     }
     var fishTracker: List<Long> = groupedFish
-    (0 until 256).forEach { dayNumber ->
+    repeat(256) {
         val birthFish = fishTracker[0]
         fishTracker = listOf(
-            fishTracker[1],
-            fishTracker[2],
-            fishTracker[3],
-            fishTracker[4],
-            fishTracker[5],
-            fishTracker[6],
-            fishTracker[7] + birthFish,
-            fishTracker[8],
-            birthFish
+            fishTracker[1], // 0
+            fishTracker[2], // 1
+            fishTracker[3], // 2
+            fishTracker[4], // 3
+            fishTracker[5], // 4
+            fishTracker[6], // 5
+            fishTracker[7] + birthFish, // 6
+            fishTracker[8], // 7
+            birthFish // 8
         )
     }
     println(fishTracker.sum())
