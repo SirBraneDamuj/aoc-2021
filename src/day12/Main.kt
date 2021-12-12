4ff2cae095876ea32ac80ed576c85bd5f1ced522
Zach Thacker <zpthacker@gmail.com>
2021-12-12 01:35:42 -0500

    Day 12 refined

--- src/day12/Main.kt
+++ src/day12/Main.kt
@@ -2,92 +2,81 @@ package com.zpthacker.aoc21.day12
 
 import com.zpthacker.aoc21.getInput
 
-var connections = mutableMapOf<Cave, Set<Cave>>()
-var caves = mutableMapOf<String, Cave>()
+private var connections = mutableMapOf<Cave, Set<Cave>>()
+private var caves = mutableMapOf<String, Cave>()
 
 fun main() {
     val input = getInput(12).trim()
     val lines = input.split("\n")
     lines.fold(caves) { acc, caveLine ->
-        val (first, second) = caveLine.split("-")
-        if (first !in acc.keys) {
-            val type = if (first.uppercase() == first) {
-                CaveType.BIG
-            } else {
-                CaveType.SMALL
+        caveLine
+            .split("-")
+            .forEach { caveName ->
+                acc[caveName] = Cave(name = caveName)
             }
-            acc[first] = Cave(name = first, type = type)
-        }
-        if (second !in acc.keys) {
-            val type = if (second.uppercase() == second) {
-                CaveType.BIG
-            } else {
-                CaveType.SMALL
-            }
-            acc[second] = Cave(name = second, type = type)
-        }
         acc
     }
     lines.fold(connections) { acc, line ->
-        val (first, second) = line.split("-")
-        val firstCave = caves[first]!!
-        val secondCave = caves[second]!!
-        if (acc[firstCave] == null) {
-            acc[firstCave] = setOf(secondCave)
-        } else {
-            acc[firstCave] = acc[firstCave]!! + secondCave
-        }
-        if (acc[secondCave] == null) {
-            acc[secondCave] = setOf(firstCave)
-        } else {
-            acc[secondCave] = acc[secondCave]!! + firstCave
+        val (left, right) = line.split("-")
+        listOf(
+            left to right,
+            right to left,
+        ).forEach { (from, to) ->
+            if (to == "start") {
+                return@forEach
+            } else {
+                val fromCave = caves[from]!!
+                val toCave = caves[to]!!
+                if (acc[fromCave] == null) {
+                    acc[fromCave] = setOf(toCave)
+                } else {
+                    acc[fromCave] = acc[fromCave]!! + toCave
+                }
+            }
         }
         acc
     }
     val start = caves["start"]!!
-    val p = paths(start, mapOf(start to 2))
-    val valid = p
-        .filter { path ->
-            path
-                .split(",")
-                .groupBy { it }
-                .filter { (piece, pieces) ->
-                    if (piece.uppercase() == piece) {
-                        false
-                    } else {
-                        pieces.count() >= 2
-                    }
-                }
-                .count() < 2
-        }
-    println(p.count())
+    val part1 = paths(
+        node = start,
+        maxRevisits = 0,
+    )
+    val part2 = paths(
+        node = start,
+        maxRevisits = 1,
+    )
+    println("Day 12: Passage Pathing")
+    println("Part 1: ${part1.count()}")
+    println("Part 2: ${part2.count()}")
 }
 
-fun paths(node: Cave, smallCavesVisited: Map<Cave, Int>): List<String> {
-    return if (node.name == "end") {
-        listOf("end")
-    } else {
-        val alreadyRevisitedCave = smallCavesVisited.any { (cave, times) ->
-            cave.name != "start" && cave.name != "end" && times >= 2
+fun paths(
+    node: Cave,
+    maxRevisits: Int,
+    smallCavesVisited: Map<Cave, Int> = mapOf(),
+): List<String> {
+    return when (node.name) {
+        "end" -> {
+            listOf("end")
         }
-        if (alreadyRevisitedCave && node in smallCavesVisited.keys) {
-            return listOf()
-        }
-        val neighbors = connections[node]!! - if (alreadyRevisitedCave) smallCavesVisited.keys else setOf(caves["start"]!!)
-        if (neighbors.isEmpty()) {
-            return listOf()
-        }
-        val newVisitList = if (node.type == CaveType.SMALL && node.name != "start" && node.name != "end") {
-            smallCavesVisited + (node to (smallCavesVisited[node]?.inc() ?: 1))
-        } else {
-            smallCavesVisited
-        }
-        neighbors.flatMap { neighbor ->
-            paths(
-                node = neighbor,
-                smallCavesVisited = newVisitList
-            ).map { path ->
-                "${node.name},$path"
+        else -> {
+            val revisitLimitReached = smallCavesVisited.any { (_, times) -> times > maxRevisits }
+            if (revisitLimitReached && node in smallCavesVisited.keys) {
+                return listOf()
+            }
+            val newVisitList = if (node.type == CaveType.SMALL) {
+                smallCavesVisited + (node to (smallCavesVisited[node]?.inc() ?: 1))
+            } else {
+                smallCavesVisited
+            }
+            connections[node]!!.flatMap { neighbor ->
+                paths(
+                    node = neighbor,
+                    smallCavesVisited = newVisitList,
+                    maxRevisits = maxRevisits
+                ).map { path ->
+                    "${node.name},$path"
+                }
             }
         }
     }
@@ -98,9 +87,12 @@ enum class CaveType {
     SMALL,
 }
 
-typealias CaveConnection = Pair<Cave, Cave>
-
 data class Cave(
     val name: String,
-    val type: CaveType
-)
+) {
+    val type = if (name.uppercase() == name) {
+        CaveType.BIG
+    } else {
+        CaveType.SMALL
+    }
+}
